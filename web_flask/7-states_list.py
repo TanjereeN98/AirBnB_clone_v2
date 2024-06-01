3e6298ee5ebe0373c3fba34de1dd6d6fadbedd37
TanjereeN98 <ahmadcyc35@gmail.com>
2024-06-02 02:21:26 +0300

    Fixing task 8 V7

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,26 +1,66 @@
 #!/usr/bin/python3
-"""script to fetch states list from storage"""
-from flask import Flask, render_template
+""" module doc """
+from flask import Flask
+from flask import render_template
 from models import storage
 from models.state import State
 
 app = Flask(__name__)
 
 
-states = storage.all(State)
+@app.route("/", strict_slashes=False)
+def hello():
+    """ def doc """
+    return "Hello HBNB!"
 
 
-@app.teardown_appcontext
-def teardown(exc):
-    """close the session after each request"""
-    storage.close()
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """ def doc """
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c(text):
+    """ def doc """
+    return 'c {}'.format(text.replace("_", " "))
+
+
+@app.route('/python', defaults={'text': 'is cool'}, strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python(text):
+    """ def doc """
+    return 'Python {}'.format(text.replace("_", " "))
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def number(n):
+    """ def doc """
+    return '{} is a number'.format(n)
+
+
+@app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
+def number_odd_or_even(n):
+    """ def doc """
+    if n % 2 == 0:
+        p = 'even'
+    else:
+        p = 'odd'
+    return render_template('6-number_odd_or_even.html', number=n, parity=p)
 
 
 @app.route('/states_list', strict_slashes=False)
 def states_list():
-    """Display list of states"""
-    return render_template('7-states_list.html', states=storage.all(State))
+    """ def doc """
+    states = storage.all(State)
+    return render_template('7-states_list.html', states=states)
 
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+@app.teardown_appcontext
+def close(error):
+    """ def doc """
+    storage.close()
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
