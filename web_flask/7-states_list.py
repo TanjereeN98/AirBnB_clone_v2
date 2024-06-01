41551ae296e7b32faf8d643f1fb9f1c3a8bcdbd4
TanjereeN98 <ahmadcyc35@gmail.com>
2024-06-02 01:38:17 +0300

    Adding task 8

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""script to fetch states list from storage"""
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+
+app = Flask(__name__)
+
+
+states = storage.all(State)
+
+
+@app.route('/states_list', strict_slashes=False)
+def states_list():
+    """Display list of states"""
+    return render_template('7-states_list.html', states)
+
+
+@app.teardown_appcontext
+def tear_down():
+    """close the session after each request"""
+    storage.close()
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
