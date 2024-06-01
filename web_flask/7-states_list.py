595e2e563a9942f3c460b3e73a397f354155cc31
TanjereeN98 <ahmadcyc35@gmail.com>
2024-06-02 02:05:05 +0300

    Another try to fix task 8 - V5

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -4,8 +4,10 @@ from flask import Flask, render_template
 from models import storage
 from models.state import State
 
-
 app = Flask(__name__)
+
+
+states = storage.all(State)
 
 
 @app.teardown_appcontext
@@ -16,8 +18,8 @@ def teardown(exc):
 
 @app.route('/states_list', strict_slashes=False)
 def states_list():
-    """get state list from storage"""
-    return render_template("7-states_list.html", states=storage.all(State))
+    """Display list of states"""
+    return render_template('7-states_list.html', states=storage.all(State))
 
 
 if __name__ == '__main__':
