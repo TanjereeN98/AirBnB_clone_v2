c4274cabc1274029086c195eb88cc3165b71b5c4
TanjereeN98 <ahmadcyc35@gmail.com>
2024-06-02 04:17:23 +0300

    Adding task 9

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -13,11 +13,13 @@ def teardown(exc):
     storage.close()
 
 
-@app.route('/states_list', strict_slashes=False)
-def states_list():
+@app.route('/cities_by_states', strict_slashes=False)
+def city_by_states():
     """Display list of states"""
-    return render_template('7-states_list.html', states=storage.all(State))
+    states = storage.all(State).values()
+    return render_template('8-cities_by_states.html', states=states)
 
 
 if __name__ == '__main__':
+    """start the server"""
     app.run(host='0.0.0.0', port=5000)
